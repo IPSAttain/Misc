c0965826d75e1fe0e61133b0371c8f399cc71bee
IPSAttain <christianwagner1@yahoo.de>
2020-08-01 01:04:03 +0200

    First

--- Mittelwert/module.php
+++ Mittelwert/module.php
@@ -5,6 +5,8 @@
 		{
 			//Never delete this line!
 			parent::Create();
+
+
 		}
 
 		public function Destroy()
@@ -17,6 +19,20 @@
 		{
 			//Never delete this line!
 			parent::ApplyChanges();
+
+		$this->RegisterMessage($this->ReadPropertyInteger("SourceVariable")/, 10603  /* VM_UPDATE */);
 		}
 
+		private function Calc()
+		{
+			$buffer = explode("|",$this->GetBuffer("DataBuffer")); 
+			$index  = $buffer[0]+1;													// Index einlesen und um 1 erhöhen
+			if ($index > $this->ReadPropertyInteger("amount")) $index = 1;			// Überlauf
+			$buffer[0] = 0;
+			$buffer[$index] = $this->ReadPropertyInteger("SourceVariable");			// neuen Messwert ins Array eintragen
+			$average = array_sum($buffer) / $this->ReadPropertyInteger("amount");	// Mittelwert berechnen
+			$buffer[0] = $index;                                        // neuen Index ins Array eintragen
+			$this->SetBuffer("DataBuffer", implode("|",$buffer));					// im Infobereich der Variablen, das Array ablegen
+			SetValue($this->ReadPropertyInteger("TargetVariable"),$average);      
+		}
 	}
