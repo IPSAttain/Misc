710fbbde1a9362a8715b375d572b4d7e5c7ac4f0
IPSAttain <christianwagner1@yahoo.de>
2020-08-01 21:35:03 +0200

    Doku

--- Mittelwert/module.php
+++ Mittelwert/module.php
@@ -29,16 +29,16 @@
 		{
 			$buffer = explode("|",str_replace(',' , '.',$this->GetBuffer("DataBuffer"))); 
 			$index = $this->GetBuffer("Index"); 
-			if($index == ""){
+			if($index == ""){															//first run only
 				$index = 0;
 			}
-				if ($index >= $this->ReadPropertyInteger("amount")) $index = 0;			// Überlauf
-				$buffer[$index] = floatval(str_replace(',' , '.',$Data[0]));			// neuen Messwert ins Array eintragen
-				$average = array_sum($buffer) / count($buffer);	// Mittelwert berechnen
-				$this->SetBuffer("DataBuffer", implode("|",$buffer));					// im Infobereich der Variablen, das Array ablegen
-				$this->SetBuffer("Index", $index + 1);
+				if ($index >= $this->ReadPropertyInteger("amount")) $index = 0;			// overflow
+				$buffer[$index] = floatval(str_replace(',' , '.',$Data[0]));			// add new value
+				$average = array_sum($buffer) / count($buffer);							// count
+				$this->SetBuffer("DataBuffer", implode("|",$buffer));					// array to string to buffer
+				$this->SetBuffer("Index", $index + 1);									// Index buffer
 				$this->SendDebug("Buffer",$this->GetBuffer("DataBuffer"),0);
-				$this->SendDebug("Index",$index,0);
+				$this->SendDebug("Index",$index + 1,0);
 				$this->SendDebug("Average",$average,0);
 				SetValue($this->ReadPropertyInteger("TargetVariable"),$average);      
 
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Folgende Module beinhaltet das Misc Repository:
 
 - __Mittelwert__ ([Dokumentation](Mittelwert))  
-	Kurze Beschreibung des Moduls.
+	Berrechnung eines Mittelwertes.
